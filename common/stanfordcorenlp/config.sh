c11d3a6f038b758e5691887bead1543b2852ff81
Anderson Rossanez <anderson.rossanez@ic.unicamp.br>
2019-05-13 16:21:19 -0300

    Fixing patterns in CoreNLP's config.sh

--- common/stanfordcorenlp/config.sh
+++ common/stanfordcorenlp/config.sh
@@ -3,8 +3,26 @@
 echo "Extracting the contents of the zip file..."
 unzip stanford-corenlp-full-*.zip
 
+echo "Resolving patterns..."
+
+dir_pattern="stanford-corenlp-full-*"
+directories=( $dir_pattern )
+echo "Directory: ${directories[0]}"
+
+cd ${directories[0]}
+
+main_jar_pattern="stanford-corenlp-[0-9].[0-9].[0-9].jar"
+main_jar=( $main_jar_pattern )
+echo "Main jar: ${main_jar[0]}"
+
+models_pattern="stanford-corenlp-[0-9].[0-9].[0-9]-models.jar"
+models=( $models_pattern )
+echo "Models jar: ${models[0]}"
+
+cd ..
+
 echo "Creating the symbolic links..."
-ln -s stanford-corenlp-full-*/stanford-corenlp-[0-9]-[0-9]-[0-9].jar stanford-corenlp.jar
-ln -s stanford-corenlp-full-*/stanford-corenlp-[0-9]-[0-9]-[0-9]-models.jar stanford-corenlp-models.jar
+ln -s ${directories[0]}/${main_jar[0]} stanford-corenlp.jar
+ln -s ${directories[0]}/${models[0]} stanford-corenlp-models.jar
 
 echo "All set!"
