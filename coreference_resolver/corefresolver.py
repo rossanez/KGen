436fbe67aa51b75d35778499840d8463f24ba4ef
Anderson Rossanez <anderson.rossanez@ic.unicamp.br>
2019-05-14 14:08:57 -0300

    Adding a coreference resolver

--- coreference_resolver/corefresolver.py
+++ coreference_resolver/corefresolver.py
@@ -9,43 +9,45 @@ from sys import path
 path.insert(0, '../')
 from common.stanfordcorenlp.corenlpfactory import CoreNLPFactory
 
-class FactsExtractor:
+class CorefResolver:
 
-
-    def extract_triples(self, input_filename, verbose=False):
+    def resolve(self, input_filename, verbose=False):
         if not input_filename.startswith('/'):
             input_filename = os.path.dirname(os.path.realpath(__file__)) + '/' + filename
 
         print('Processing text from {} \nPlease wait, as it may take a while ...'.format(input_filename))
 
-        output_filename = os.path.splitext(input_filename)[0] + '_extracted_triples.txt'
+        output_filename = os.path.splitext(input_filename)[0] + '_coreferences.txt'
         open(output_filename, 'w').close()
 
         if verbose:
-            print('Searching for triples ...')
-        output = self.__openie(input_filename, output_filename, verbose)
-        print('Extracted triples were stored at {}'.format(output))
+            print('Resolving coreferences ...')
+        output = self.__dcoref(input_filename, output_filename, verbose)
+        print('Resolved coreferences were stored at {}'.format(output))
 
         return output
 
-    def __openie(self, input, output, verbose=False):
+    def __dcoref(self, input, output, verbose=False):
         with open(input, 'r') as input_file:
             contents = input_file.read()
             input_file.close()
 
         nlp = CoreNLPFactory.createCoreNLP()
-        annotated = nlp.annotate(contents, properties={'annotators': 'tokenize, ssplit, pos, ner, depparse, parse, openie', 'outputFormat': 'json'})
+        annotated = nlp.annotate(contents, properties={'annotators': 'tokenize, ssplit, pos, lemma, ner, parse, dcoref', 'outputFormat': 'json'})
 
         json_output = json.loads(annotated)
 
-        for sentence in json_output['sentences']:
-            for openie in sentence['openie']:
-                with open(output, 'a') as output_file:
-                    triple = '{}:({};{};{})'.format(sentence['index'], openie['subject'], openie['relation'], openie['object'])
-                    if verbose:
-                       print(triple)
-                    output_file.write(triple + '\n')
-                    output_file.close()
+        for k, mentions in json_output['corefs'].items():
+            simplified_mentions = ''
+            for m in mentions:
+#                simplified_mentions(m['sentNum'], m['startIndex'], m['endIndex'], m['text']))
+                simplified_mentions += '{}:{};'.format(m['sentNum'] -1, m['text'])
+
+            if verbose:
+                print(simplified_mentions)
+            with open(output, 'a') as output_file:
+                output_file.write(simplified_mentions + '\n')
+                output_file.close()
 
         return output
 
@@ -62,8 +64,8 @@ def main(args):
         print('No file provided.')
         exit(1)
 
-    extractor = FactsExtractor()
-    extractor.extract_triples(filename, verbose)
+    coref = CorefResolver()
+    coref.resolve(filename, verbose)
 
 if __name__ == '__main__':
     exit(main(argv))
