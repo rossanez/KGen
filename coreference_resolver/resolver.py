5350004d14471381c4045dd4cfe1a971b6b34b3b
Anderson Rossanez <anderson.rossanez@ic.unicamp.br>
2019-05-29 20:15:35 -0300

    Implementing abbrev detection in the coreferences detector

--- coreference_resolver/resolver.py
+++ coreference_resolver/resolver.py
@@ -2,6 +2,7 @@ import json
 import os
 
 from argparse import ArgumentParser
+from nltk.tree import ParentedTree
 from sys import argv
 from sys import path
 
@@ -15,8 +16,11 @@ class CorefResolver:
             input_filename = os.path.dirname(os.path.realpath(__file__)) + '/' + input_filename
 
         print('Processing text from {}'.format(input_filename))
+        with open(input_filename, 'r') as input_file:
+            contents = input_file.read()
+            input_file.close()
 
-        resolved_contents = self.__coref(input_filename, verbose)
+        resolved_contents = self.__resolve_possessives(self.__resolve_abbrevs(self.__coref(contents, verbose), verbose))
 
         output_filename = os.path.splitext(input_filename)[0] + '_resolved.txt'
         with open(output_filename, 'w') as output_file:
@@ -27,11 +31,50 @@ class CorefResolver:
 
         return output_filename
 
-    def __coref(self, input, verbose=False):
-        with open(input, 'r') as input_file:
-            contents = input_file.read()
-            input_file.close()
+    def __resolve_abbrevs(self, contents, verbose=False):
+        if verbose:
+            print('Looking for abbreviations and their occurences...')
+        print('Please wait, as it may take a while ...')
 
+        nlp = CoreNLPFactory.createCoreNLP()
+        annotated = nlp.annotate(contents, properties={'annotators': 'tokenize, ssplit, pos, lemma, ner, parse', 'outputFormat': 'json'})
+
+        json_output = json.loads(annotated)
+
+        abbrev_refs = {}
+        for sentence in json_output['sentences']:
+            parsed = sentence['parse'].replace('\n', '')
+            parse_tree = ParentedTree.fromstring(parsed)
+            for sub_tree in parse_tree.subtrees():
+                if sub_tree.label() == 'PRN':
+                    if sub_tree.parent().label() == 'NP' and sub_tree.left_sibling().label() == 'NP':
+                        abbrev = ' '.join(sub_tree.leaves()).strip()
+                        abbrev = abbrev[abbrev.find('-LRB-') + 5:abbrev.find('-RRB-')].strip()
+
+                        left = sub_tree.left_sibling()
+                        reference = ' '.join(left.leaves()).strip()
+
+                        if verbose:
+                            print('- {} : {}'.format(abbrev, reference))
+
+                        abbrev_refs[abbrev] = reference
+                        if abbrev.endswith('s') and reference.endswith('s'): #Referring to a plural abbreviation? Ouch!
+                            abbrev_refs[abbrev[:-1]] = reference[:-1]
+
+        contents = ''
+        for sentence in json_output['sentences']:
+            for token in sentence['tokens']:
+                if token['word'] in abbrev_refs:
+                    contents += abbrev_refs[token['word']] + ' '
+                else:
+                    contents += token['word'] + ' '
+
+        for key in abbrev_refs:
+            contents = contents.replace('{} -LRB- {} -RRB-'.format(abbrev_refs[key], abbrev_refs[key]), abbrev_refs[key])
+
+        return contents
+
+    def __coref(self, contents, verbose=False):
         if verbose:
             print('Resolving coreferences with Stanford corefs annotator.')
         print('Please wait, as it may take a while ...')
@@ -41,7 +84,7 @@ class CorefResolver:
 
         json_output = json.loads(annotated)
 
-        return self.__resolve_possessives(self.__rebuild_contents(json_output['sentences'], self.__eval_corefs(json_output['corefs'], verbose)))
+        return self.__rebuild_contents(json_output['sentences'], self.__eval_corefs(json_output['corefs'], verbose))
 
     def __eval_corefs(self, json_corefs, verbose=False):
         corefs = {}
