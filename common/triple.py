d0080690c257c1919f48783c4a63f5370b748d37
Anderson Rossanez <anderson.rossanez@ic.unicamp.br>
2019-06-15 12:45:44 -0300

    Fixing undesired characters in ttl

--- common/triple.py
+++ common/triple.py
@@ -10,22 +10,25 @@ class Triple:
        self.__predicate = p
        self.__object = o
 
+   def __format_name(self, name):
+       return name.replace(' ', '_').replace('\'', '')
+
    def get_turtle(self):
        prefixes = {'http://www.w3.org/1999/02/22-rdf-syntax-ns#': 'rdf', 'http://www.w3.org/2000/01/rdf-schema#': 'rdfs', 'http://local/local.owl#': 'local'}
        classes = {}
        properties = {}
 
-       s = 'local:{}'.format(self.__subject.replace(' ', '_'))
+       s = 'local:{}'.format(self.__format_name(self.__subject))
        s_class = '{}\ta\trdfs:Class'.format(s)
        s_label = 'rdfs:label\t"{}"'.format(self.__subject)
        classes.update({s: '{}\t;\n\t{}\t.'.format(s_class, s_label)})
 
-       o = 'local:{}'.format(self.__object.replace(' ', '_'))
+       o = 'local:{}'.format(self.__format_name(self.__object))
        o_class = '{}\ta\trdfs:Class'.format(o)
        o_label = 'rdfs:label\t"{}"'.format(self.__object)
        classes.update({o: '{}\t;\n\t{}\t.'.format(o_class, o_label)})
 
-       p = 'local:{}'.format(self.__predicate.replace(' ', '_'))
+       p = 'local:{}'.format(self.__format_name(self.__predicate))
        p_class = '{}\ta\trdf:Property'.format(p)
        p_domain = 'rdfs:domain\t{}'.format(s)
        p_range = 'rdfs:range\t{}'.format(o)
@@ -35,6 +38,3 @@ class Triple:
        relation = '{}\t{}\t{}\t.'.format(s, p, o)
 
        return prefixes, classes, properties, relation
-
-       
-
